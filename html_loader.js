723c86a18cb41a4527291f5953f617ca38c35f37
inevitablegoose <inevitablegoose@gmail.com>
2025-09-03 21:39:10 -0600

    Uploading Files from Neocities

---
+++ html_loader.js
@@ -0,0 +1,45 @@
+///
+// HTML Loader by netfriend
+// Released under the Unlicense - https://unlicense.org/
+///
+
+///
+// SETTINGS
+///
+
+const html_templates = {
+  "linklist": "/resources/header.html",
+  "social": "/resources/social.html",
+};
+
+///
+// CODE
+///
+// Don't edit below this line unless you know what you're doing!
+///
+
+(function () {
+
+const domParser = new window.DOMParser();
+
+Object.keys(html_templates).map(function (element) {
+  const url = html_templates[element];
+
+  fetch(url).then((response) => response.text())
+    .then(function (html) {
+      let parsedHTML = domParser.parseFromString(html, "text/html");
+      let pageElement = document.getElementById(element);
+
+      if (pageElement.replaceChildren !== undefined) {
+        pageElement.replaceChildren(...parsedHTML.body.childNodes);
+      } else {
+        while (pageElement.firstChild) {
+          pageElement.removeChild(pageElement.firstChild);
+        }
+        pageElement.append(...parsedHTML.body.childNodes);
+      }
+      document.head.appendChild(parsedHTML.head);
+    });
+});
+
+}());
